3d0e09496ddcdb93b8e541ecfcd52fb6f1c76136
Guy Starbuck <gstarbuck@wisc.edu>
2016-04-13 12:17:01 -0500

    Initial implementation work, still working on Access dbconn

---
+++ GenFlatFile/FileRow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFlatFile
+{
+    public class FileRow
+    {
+        public string Barcode { get; set; }
+        public string ScanID { get; set; }
+        public string ScanDate { get; set; }
+        public string ScannerName { get; set; }
+    }
+}
--- GenFlatFile/Program.cs
+++ GenFlatFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,36 @@ namespace GenFlatFile
         static void Main(string[] args)
         {
             /* Get data from Access */
+            //var dbConnStr = @"Provier=Microsoft.ACE.OLEDB.15.0;Data Source=C:\Users\Guy Starbuck\Documents\Database1.accdb";
+            var dbConnStr = @"Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\Users\Guy Starbuck\Documents\Database1.accdb";
+            
+            var conn = new OdbcConnection(dbConnStr);
 
+            conn.Open();
 
+            OdbcCommand cmd = new OdbcCommand("SELECT * FROM Scanners INNER JOIN ScansTable ON Scanners.ID = ScansTable.Scanner");
+
+            var reader = cmd.ExecuteReader();
 
             /* Load into POCO structure */
+            var rows = new SortedSet<FileRow>();
+
+            while (reader.Read())
+            {
+                var barcode = reader.GetString(2) + "-" + reader.GetString(3);
+                var scannerName = reader.GetString(1);
+                var scanDate = reader.GetString(4);
+                var scanId = reader.GetString(2);
+
+                var row = new FileRow()
+                {
+                    Barcode = barcode,
+                    ScannerName = scannerName,
+                    ScanDate = scanDate,
+                    ScanID = scanId 
+                };
+                rows.Add(row);
+            }
 
             /* Write out to CSV */
         }
