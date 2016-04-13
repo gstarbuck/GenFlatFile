ba9211e2f9a2cccd310b3c84aadc579d76729b53
Guy Starbuck <gstarbuck@wisc.edu>
2016-04-13 10:46:12 -0500

    Initial Commit

---
+++ GenFlatFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFlatFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Get data from Access */
+
+
+
+            /* Load into POCO structure */
+
+            /* Write out to CSV */
+        }
+    }
+}
